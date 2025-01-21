8099a445ab596a1c9b2efc9985b80c7a19eb2566
sribabu <sribabumandraju@gmail.com>
2025-01-21 22:47:12 +0530

    new changes

--- routes/crackedRoutes.js
+++ routes/crackedRoutes.js
@@ -1,9 +1,10 @@
 import express from "express";
 import { checkPassword, setPresetPassword,isPasswordCracked } from "../controllers/crackController.js";
+import { ipRateLimiter } from "../middleware/rateLimitMiddleware.js";
 
 const router = express.Router();
 
-router.post("/check-password", checkPassword);
+router.post("/check-password",ipRateLimiter, checkPassword);
 router.post("/set-preset-password", setPresetPassword);
 router.get("/isCracked",isPasswordCracked)
 
